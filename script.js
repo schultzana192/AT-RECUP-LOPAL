ec07a0c4eeeaff2b618b8ebd42322a6d3421cd07
Aluno <schultzana192@gmail.com>
2026-09-19 10:22:00 -0300

    realizado o ex5

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-let soma = 0;
+let saldo = 1000;
+let opcao;
 
-for (let i = 1; i <= 100; i++) {
-    soma += i;
-}
-alert(soma);
+do {
+    opcao = Number(prompt("1 - Depósito\n",  "2 - Saque\n", "3 - Encerrar"
+    ));
+
+    switch (opcao) {
+
+        case 1:
+            let deposito = Number(prompt("Digite o valor do depósito"));
+            saldo += deposito;
+            alert("Saldo atualizado: R$ " + saldo);
+            break;
+
+        case 2:
+            let saque = Number(prompt("Digite o valor do saque"));
+
+            if (saque <= saldo) {
+                saldo -= saque;
+                alert("Saldo atualizado: R$ " + saldo);
+            } else {
+                alert("Saldo insuficiente");
+            }
+            break;
+
+        case 3:
+            alert("Programa encerrado");
+            break;
+
+        default:
+            alert("Opção inválida");
+    }
+
+} while (opcao != 3);
+
 
 
 
